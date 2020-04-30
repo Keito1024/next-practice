f9bd91db5590749bb84fa549388d2b10c34833c9
Keito1024 <kkrt0724@gmail.com>
2020-05-01 02:05:33 +0900

    deal with typescript

--- nextjs-blog/components/layout.tsx
+++ nextjs-blog/components/layout.tsx
@@ -1,12 +1,19 @@
-import Head from 'next/head'
-import styles from './layout.module.css'
-import utilStyles from '../styles/utils.module.css'
-import Link from 'next/link'
+import Head from 'next/head';
+import Link from 'next/link';
 
-const name = 'Keito Katsuta'
+import utilStyles from '../styles/utils.module.css';
+import styles from './layout.module.css';
+
+const name = '[Your Name]'
 export const siteTitle = 'Next.js Sample Website'
 
-export default function Layout({ children, home }) {
+export default function Layout({
+  children,
+  home
+}: {
+  children: React.ReactNode
+  home?: boolean
+}) {
   return (
     <div className={styles.container}>
       <Head>
